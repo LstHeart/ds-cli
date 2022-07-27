5487f1167802e5fed75d2dd3066acd77b811415a
lstheart <1042138926@qq.com>
2022-07-28 01:28:43 +0800

    test: update scripts

--- .devcontainer/scripts/custom/setup_npm.sh
+++ .devcontainer/scripts/custom/setup_npm.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-# switch nodejs && set default nodejs
-nvm use 14 && nvm default 14
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+# # switch nodejs && set default nodejs
+# nvm use 14 && nvm default 14
 
 
 # custom global npm
