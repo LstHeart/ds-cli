ab7503e8681620cc6000e55764fc9fa96ec68c87
LstHeart <dennis.lstheart@qq.com>
2022-07-27 17:34:30 +0000

    update

--- .devcontainer/scripts/custom/setup_npm.sh
+++ .devcontainer/scripts/custom/setup_npm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# export NVM_DIR="$HOME/.nvm"
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 # # switch nodejs && set default nodejs
 # nvm use 14 && nvm default 14
 
