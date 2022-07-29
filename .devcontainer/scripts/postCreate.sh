d90a4949d31b12ca711f57c82c4adbaf2250b1b9
LstHeart <dennis.lstheart@qq.com>
2022-07-29 22:15:33 +0800

    update script

--- .devcontainer/scripts/postCreate.sh
+++ .devcontainer/scripts/postCreate.sh
@@ -1,28 +1,11 @@
 #!/bin/sh
 
-# It happens after updateContentCommand and once the dev container has been assigned to a user for the first time(execute once)
+# 开发容器分配给用户的时候执行
 echo `date +"[%Y/%m/%d %H:%M:%S]"` 'postCreateCommand will begin✨' \
   && echo '[working-dir]:' `pwd`
 
-# ln -sf $HOME/config $PWD/.devcontainer/config && set +x
-
-## setup fish
-# if [ ! -d ~/.config/fish/conf.d ]; then
-#   mkdir -p ~/.config/fish/conf.d
-# fi
-# curl https://raw.githubusercontent.com/skywind3000/z.lua/master/z.lua > ~/.config/fish/conf.d/z.lua \
-# && echo "lua ~/.config/fish/conf.d/z.lua --init fish | source" > ~/.config/fish/conf.d/z.fish
-
-# fish -c "fisher install jorgebucaran/nvm.fish"
-# fish -c "fisher install PatrickF1/fzf.fish"
-# fish -c "fisher install franciscolourenco/done "
-# fish -c "fisher install jorgebucaran/autopair.fish"
-# fish -c "fisher install andreiborisov/sponge"
-# fish -c "echo y | fisher install ilancosman/tide@v5"
-
-# # git-completion
-# wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash \
-#   -O $HOME/.git-completion.bash
+# 更新项目依赖
+ni
 
 echo `date +"[%Y/%m/%d %H:%M:%S]"` 'postCreateCommand has done🎉'
 exit
--- .devcontainer/scripts/postStart.sh
+++ .devcontainer/scripts/postStart.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
 # A command to run each time the container is successfully started (execute everytime)
+# 一般用于项目启动
 echo `date +"[%Y/%m/%d %H:%M:%S]"` 'postStartCommand will begin✨' \
   && echo '[working-dir]:' `pwd`
 
 # copy dotfiles log if exist
 DofilesLog='/workspaces/.codespaces/.persistedshare/dotfiles/dotfiles.log'
-[ -f $DofilesLog ] && cp -f $DofilesLog .devcontainer/logs/
+[ -f $DofilesLog ] && [ ! -d .devcontainer/logs/] && cp -f $DofilesLog .devcontainer/logs/
 
 # cat ~/.bashrc
 ## update project dependencies and so on
 # echo 'update dependencies' && ni
 # nr serve
-# nr build
+nr build
 ## ...
 
 echo `date +"[%Y/%m/%d %H:%M:%S]"` 'postStartCommand has done🎉'
