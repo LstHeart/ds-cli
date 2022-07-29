660a636e036de0f10fe54a9bcedf6265fb7a86d0
LstHeart <dennis.lstheart@qq.com>
2022-07-29 10:40:50 +0800

    chore: clean code

--- .devcontainer/scripts/updateContent.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-# executes inside the container after onCreateCommand(execute at least once)
-echo `date +"[%Y/%m/%d %H:%M:%S]"` 'updateContentCommand will begin✨' \
-  && echo '[working-dir]:' `pwd`
-
-
-echo `date +"[%Y/%m/%d %H:%M:%S]"` 'updateContentCommand has done🎉'
-exit
